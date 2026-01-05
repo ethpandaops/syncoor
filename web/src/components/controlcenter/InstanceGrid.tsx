c13d75b79e43cc964de5f2d818dcece62fb779fc
Rafael Matias <rafael@skyle.net>
2026-01-05 16:34:21 +0100

    feat(control-center): show up to 5 instance cards per row

--- web/src/components/controlcenter/InstanceGrid.tsx
+++ web/src/components/controlcenter/InstanceGrid.tsx
@@ -40,7 +40,7 @@ const InstanceGrid: React.FC<InstanceGridProps> = ({ instances, isLoading, error
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       {instances.map((instance) => (
         <InstanceStatusCard key={instance.name} instance={instance} />
       ))}
